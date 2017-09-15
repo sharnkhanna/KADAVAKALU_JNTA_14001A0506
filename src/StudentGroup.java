08b327e8bd0b8b43313bd793d36fae342f3eba96
sharnkhanna <sharn.kumar.09@gmail.com>
2017-09-15 18:57:17 +0530

    fg

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -32,6 +32,9 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public void setStudents(Student[] students) {
 		// Add your implementation here
+		if(students==null)
+			throw new IllegalArgumentException();
+		this.students = students;
 	}
 
 	@Override
